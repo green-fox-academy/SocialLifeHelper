100bd055daf3cc8050cb66e5963de803150dc56e
barbarasimandi <barbarasimandi@gmail.com>
2018-01-10 14:04:05 +0100

    dependencies updated

--- src/main/java/com/greenfox/blackjackbot/BlackjackbotApplication.java
+++ src/main/java/com/greenfox/blackjackbot/BlackjackbotApplication.java
@@ -1,10 +1,29 @@
 package com.greenfox.blackjackbot;
 
+import com.github.messenger4j.MessengerPlatform;
+import com.github.messenger4j.send.MessengerSendClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlackjackbotApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(BlackjackbotApplication.class);
+
+	/**
+	 * Initializes the {@code MessengerSendClient}.
+	 *
+	 * @param pageAccessToken the generated {@code Page Access Token}
+	 */
+	@Bean
+	public MessengerSendClient messengerSendClient(@Value("${messenger4j.pageAccessToken}") String pageAccessToken) {
+		logger.debug("Initializing MessengerSendClient - pageAccessToken: {}", pageAccessToken);
+		return MessengerPlatform.newSendClientBuilder(pageAccessToken).build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlackjackbotApplication.class, args);
