2d57848bdd53a759a58857484aa7571adc905d27
barbarasimandi <barbarasimandi@gmail.com>
2018-01-10 13:21:03 +0100

    gitignore updated, project generated

---
+++ src/main/java/com/greenfox/blackjackbot/BlackjackbotApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.blackjackbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackbotApplication.class, args);
+	}
+}
